e9bd6befff2aab7e261e2fe7df09f34d76a436e6
woomchi <89631875+woomchi@users.noreply.github.com>
2025-12-31 00:27:48 +0900

    Queue 예제 코드 수정

--- 자바의정석/QueueEx1.java
+++ 자바의정석/QueueEx1.java
@@ -20,7 +20,7 @@ public class QueueEx1 {
                     System.exit(0);
                 }else if(input.equalsIgnoreCase("help")){
                     System.out.println(" help - 도움말을 보여줍니다.");
-                    System.out.println(" q Ehsms Q - 프로그램을 종룧바니다.");
+                    System.out.println(" q 또는 Q - 프로그램을 종료합니다.");
                     System.out.println(" history - 최근에 입력된 명령어를 " + MAX_SIZE+"개 보여줍니다.");
                 } else if(input.equalsIgnoreCase("history")){
                     int i = 0;
