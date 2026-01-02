f4a5e8ae3dd3361ce356ec137bdcb30a0eeb10f0
woomchi <hello.git.hitszombie@gmail.com>
2026-01-02 14:46:19 +0900

    TreeSet 예제 코드

---
+++ 자바의정석/AsciiPrint.java
@@ -0,0 +1,10 @@
+package 자바의정석;
+
+public class AsciiPrint {
+    public static void main(String[] args) {
+        char ch = ' ';
+        for(int i=0;i<95;i++){
+            System.out.print(ch++);
+        }
+    }
+}
