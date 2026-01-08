13f634ee38cf77b65dc566b883a8bc5e01e5ed04
woomchi <89631875+woomchi@users.noreply.github.com>
2026-01-08 15:21:12 +0900

    String 클래스 예제

---
+++ study/StringClass/StringEx3.java
@@ -0,0 +1,17 @@
+package study.StringClass;
+import java.util.StringJoiner;
+
+public class StringEx3 {
+    public static void main(String[] args) {
+        String animals = "dog,cat,bear";
+        String [] arr = animals.split(",");
+
+        System.out.println(String.join("-",arr));
+
+        StringJoiner sj = new StringJoiner("/","[","]");
+        for(String s: arr){
+            sj.add(s);
+        }
+        System.out.println(sj.toString());
+    }
+}
