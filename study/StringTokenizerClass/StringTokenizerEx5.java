8981c47e01391d8078fc151546772090bd53fba0
woomchi <hello.git.hitszombie@gmail.com>
2026-01-12 16:51:30 +0900

    StringTokenizer 예제 코드

---
+++ study/StringTokenizerClass/StringTokenizerEx5.java
@@ -0,0 +1,22 @@
+package study.StringTokenizerClass;
+import java.util.*;
+
+public class StringTokenizerEx5 {
+    public static void main(String[] args) {
+        String data = "100,,,200,300";
+
+        String [] result = data.split(",");
+        StringTokenizer st = new StringTokenizer(data,",");
+
+        for(int i=0;i<result.length;i++){
+            System.out.print(result[i]+"|");
+        }
+        System.out.println("개수:"+result.length);
+
+        int i=0;
+        for(;st.hasMoreElements();i++){
+            System.out.print(st.nextToken()+"|");
+        }
+        System.out.println("개수:"+i);
+    }
+}
