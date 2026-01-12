3bfd06c13c04200524fbdbdbed6461f053ea437b
woomchi <89631875+woomchi@users.noreply.github.com>
2026-01-12 16:51:30 +0900

    StringTokenizer 예제 코드

---
+++ study/StringTokenizerClass/StringTokenizerEx2.java
@@ -0,0 +1,13 @@
+package study.StringTokenizerClass;
+import java.util.*;
+
+public class StringTokenizerEx2 {
+    public static void main(String[] args) {
+        String expression = "x=100*(200+300)/2";
+        StringTokenizer st = new StringTokenizer(expression, "+-*/=()",true);
+        
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
+    }
+}
